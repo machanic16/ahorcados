a64f9763f80564eb7fe09f40e836ce989395a513
Joel <jatr160397@gmail.com>
2019-11-24 12:06:59 -0500

    se implemento el camino de ganar el juego, pero hay varios bug en la logica

--- ahorcados.py
+++ ahorcados.py
@@ -69,7 +69,6 @@ IMAGES = ['''
         =========''', '''
 ''']
 
-tries = 0
 WORD_LIST = ['CASA', 
              'PERRO',
              'COMPUTADORA',
@@ -79,12 +78,14 @@ WORD_LIST = ['CASA',
              'LIBERTAD']
 
 def random_word(WORD_LIST):
+    ''' This function return a random word from word list'''
     random_index = random.randint(0,len(WORD_LIST) - 1)
     return WORD_LIST[random_index]
 
 
 
 def show_dashboar(hiden_word, tries):
+    ''' Show the dashboard'''
     print(IMAGES[tries])
     print(hiden_word)
         
@@ -92,8 +93,11 @@ def show_dashboar(hiden_word, tries):
 
 
 def run():
+    ''' Starts the game'''
+    tries = 0
     word = random_word(WORD_LIST)
-    hiden_word = ('*-*') * len(word)
+    hiden_word = ['*-*'] * len(word)
+    non_alpha_type = 0
     
     while True:
         
@@ -114,7 +118,33 @@ def run():
                     idx_to_change.append(idx_in_word)
                     print(idx_to_change)
                 idx_in_word += 1 
+            
+            for idx in idx_to_change:
+                hiden_word[idx] = current_letter
+            
+            try:
+                hiden_word.index('*-*')
+            except ValueError:
+                print('')
+                print(' Fleicidades ganaste, la palabra es {}'.format(word))
+                break
+        
+        elif current_letter.isalpha() :
+            if non_alpha_type == 0:
+                print('Solo puedes usar letras, de ahora en adelante estos tambien contaran como errores')
+                non_alpha_type += 1
+            else:
+                tries += 1
 
+        else:
+            print('La letra {} NO esta en la palabra')
+            if tries < len(IMAGES) - 1:
+                tries += 1
+            else:
+                print('Perdiste .-.')
+                break            
+
+    #show_dashboar('hola', tries)
 
 if __name__ == '__main__':
 
---
+++ readme.md
@@ -0,0 +1,3 @@
+Este es un script de un peque;o juego del ahorcado
+un proyecto para dominar las bases de python, estoy abrierto a sugerencias
+o cambios, es algo divertido para tener en la consola
